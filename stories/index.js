c8d29c2bbaf927a465f527dbf9197d3d4fd7b67f
Diego Borges <drborges.cic@gmail.com>
2017-11-05 18:58:26 -0200

    Fix sotrybook stories

--- stories/index.js
+++ stories/index.js
@@ -8,14 +8,14 @@ import Validation from "../src/Validation";
 import styles from "./styles.scss";
 
 const html5RequiredInput = () => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
     <label htmlFor="username">* Username: </label>
     <input id="username" type="text" required />
   </Validation>
 );
 
 const html5RequiredInputCheckbox = () => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
     <label htmlFor="username">* Username: </label>
     <input type="checkbox" name="username" value="1" required />drborges
     <input type="checkbox" name="username" value="2" required />diego
@@ -24,7 +24,7 @@ const html5RequiredInputCheckbox = () => (
 );
 
 const html5RequiredInputRadio = () => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
     <label htmlFor="username">* Username: </label>
     <input type="radio" name="username" value="1" required />drborges
     <input type="radio" name="username" value="2" required />diego
@@ -33,7 +33,7 @@ const html5RequiredInputRadio = () => (
 );
 
 const html5RequiredSelect = () => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
     <label htmlFor="username">* Username: </label>
     <select required>
       <option />
@@ -45,62 +45,62 @@ const html5RequiredSelect = () => (
 );
 
 const html5RequiredTextarea = () => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
     <label htmlFor="username">* Username: </label>
     <textarea required />
   </Validation>
 );
 
 const html5EmailInput = () => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
     <label htmlFor="email">Email: </label>
     <input id="email" type="email" />
   </Validation>
 );
 
 const html5URLInput = () => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
     <label htmlFor="website">Website: </label>
     <input id="website" type="url" />
   </Validation>
 );
 
 const html5NumberInput = () => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
     <label htmlFor="age">Age (between 18 and 40): </label>
     <input id="age" type="number" min="18" max="40" step="1" />
   </Validation>
 );
 
 const html5InputPattern = () => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
     <label htmlFor="phone">Cell: </label>
     <input id="phone" type="text" pattern="\(\d\d\d\) \d\d\d-\d\d\d\d" placeholder="(ddd) ddd-dddd" />
   </Validation>
 );
 
 const throttleValidationBy = (delay) => (
-  <Validation onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)} throttle={delay}>
+  <Validation onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()} throttle={delay}>
     <label htmlFor="username">* Username: </label>
     <input id="username" type="text" minLength={3} maxLength={10} />
   </Validation>
 );
 
 const nonZeroAndEvenInput = () => {
-  const nonZeroNumber = (event) => new Promise((resolve, reject) =>
-    (event.target.value != 0) ?
-      resolve(event) :
+  const nonZeroNumber = (value) => new Promise((resolve, reject) =>
+    (value != 0) ?
+      resolve(value) :
       reject("Cannot be zero!")
   );
 
-  const evenNumber = (event) => new Promise((resolve, reject) =>
-    (event.target.value % 2 == 0) ?
-      resolve(event) :
+  const evenNumber = (value) => new Promise((resolve, reject) =>
+    (value % 2 == 0) ?
+      resolve(value) :
       reject("Must be an even number!")
   );
 
   return (
-    <Validation rules={[ nonZeroNumber, evenNumber ]} onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+    <Validation rules={[ nonZeroNumber, evenNumber ]} onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
       <label htmlFor="nonzero-even">Non-zero and even: </label>
       <input id="nonzero-even" type="number" />
     </Validation>
@@ -108,15 +108,15 @@ const nonZeroAndEvenInput = () => {
 };
 
 const githubUsernameInput = () => {
-  const githubUsernameExists = (event) => new Promise(async (resolve, reject) => {
-    const response = await fetch(`https://api.github.com/users/${event.target.value}`);
+  const githubUsernameExists = (value) => new Promise(async (resolve, reject) => {
+    const response = await fetch(`https://api.github.com/users/${value}`);
     response.ok ?
-      resolve(event) :
-      reject(`Username '${event.target.value}' does not exist!`);
+      resolve(value) :
+      reject(`Username '${value}' does not exist!`);
   });
 
   return (
-    <Validation rules={[ githubUsernameExists ]} throttle={700} onSuccess={action("Valid!")} onFailure={(e) => action(e.target.validationMessage)(e)}>
+    <Validation rules={[ githubUsernameExists ]} throttle={700} onValid={(value) => action("Valid:", value)} onInvalid={(hint) => action(hint)()}>
       <label htmlFor="gh-username">Github Username: </label>
       <input id="gh-username" type="text" required />
     </Validation>
@@ -143,7 +143,8 @@ const formWithControlledState = () => {
 
     handleChange = (event) => {
       this.setState({
-        [event.target.name]: event.target.type === "checkbox" ? event.target.checked : event.target.value,
+        [event.target.name]: event.target.type === "checkbox" ?
+          event.target.checked : event.target.value,
       })
     }
 
@@ -176,29 +177,29 @@ const formWithControlledState = () => {
     <Form />
   );
 };
-//
-// storiesOf("Validation/HTML5", module)
-//   .add("required", () => html5RequiredInput())
-//   .add("email", () => html5EmailInput())
-//   .add("url", () => html5URLInput())
-//   .add("number", () => html5NumberInput())
-//   .add("input pattern", () => html5InputPattern());
-//
-// storiesOf("Validation/Custom", module)
-//   .add("with multiple rules", () => nonZeroAndEvenInput())
-//   .add("Github API validation", () => githubUsernameInput());
-//
-// storiesOf("Validation/Supported Fields", module)
-//   .add("input", () => html5RequiredInput())
-//   .add("checkbox", () => html5RequiredInputCheckbox())
-//   .add("radio", () => html5RequiredInputRadio())
-//   .add("select", () => html5RequiredSelect())
-//   .add("textarea", () => html5RequiredTextarea());
-//
-// storiesOf("Validation/Throtteled", module)
-//   .add("by 100ms", () => throttleValidationBy(100))
-//   .add("by 500ms", () => throttleValidationBy(500))
-//   .add("by 1s", () => throttleValidationBy(1000));
+
+storiesOf("Validation/HTML5", module)
+  .add("required", () => html5RequiredInput())
+  .add("email", () => html5EmailInput())
+  .add("url", () => html5URLInput())
+  .add("number", () => html5NumberInput())
+  .add("input pattern", () => html5InputPattern());
+
+storiesOf("Validation/Custom", module)
+  .add("with multiple rules", () => nonZeroAndEvenInput())
+  .add("Github API validation", () => githubUsernameInput());
+
+storiesOf("Validation/Supported Fields", module)
+  .add("input", () => html5RequiredInput())
+  .add("checkbox", () => html5RequiredInputCheckbox())
+  .add("radio", () => html5RequiredInputRadio())
+  .add("select", () => html5RequiredSelect())
+  .add("textarea", () => html5RequiredTextarea());
+
+storiesOf("Validation/Throtteled", module)
+  .add("by 100ms", () => throttleValidationBy(100))
+  .add("by 500ms", () => throttleValidationBy(500))
+  .add("by 1s", () => throttleValidationBy(1000));
 
 storiesOf("Validation/Use Cases", module)
   .add("Password Confirmation", () => formWithControlledState());
