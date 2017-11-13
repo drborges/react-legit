564511863b01ac69e353cf37ad8302b32e6cf367
Diego Borges <drborges.cic@gmail.com>
2017-11-13 00:26:57 -0200

    Add module alias to simplify imports

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "../src"),
+      "../node_modules",
+    ],
+  },
   module: {
     rules: [
       {
