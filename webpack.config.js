881933c3cf920fc98daca2d2f5dc6804a9aeb081
Diego Borges <drborges.cic@gmail.com>
2017-11-24 11:16:10 -0200

    Use JSON stringify to set NODE_ENV value

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': `"${env}"`,
+      'process.env.NODE_ENV': JSON.stringify(env),
     }),
     new CompressionPlugin({
       asset: "[path].gz[query]",
